e975a9bb3348bf50a42bad412ddfb1714fe1038a
fionacfeng <ffeng@katalystadvantage.com>
2021-11-30 09:26:23 -0500

    create signup page

---
+++ gatsby-starter-default/src/pages/sign-up.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+
+const SignUpPage = () => (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Sign Up</h1>
+    </Layout>
+  )
+  
+  export default SignUpPage
