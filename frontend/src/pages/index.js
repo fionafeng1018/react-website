7c629a637d6c6c505b6c426bfcd32a059012ba86
fionacfeng <ffeng@katalystadvantage.com>
2021-12-06 21:18:34 -0500

    config tailwindcss

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -20,11 +20,10 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/signup/">Go to Sign Up</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link><br />
-      <Link to="/sign-up">Sign up</Link>
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
   </Layout>
 )
